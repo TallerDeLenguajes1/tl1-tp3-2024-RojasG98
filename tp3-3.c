a29c21a0ddbcee35f2800e5044eb98d95ef3a46a
GabielR98 <rojista.098g@gmail.com>
2024-05-01 17:46:44 -0300

    Resolucion ejercicio 5

--- tp3-3.c
+++ tp3-3.c
@@ -7,17 +7,22 @@ int main()
 {
     char *buff;
     char **nombres;
-    nombres = (char **)malloc(sizeof(char *) * TAMA);
+    int cantNombres;
+    printf("Ingrese la cantidad de nombres: ");
+    scanf("%d",&cantNombres);
+
+    nombres = (char **)malloc(sizeof(char *) * cantNombres);
     buff = (char *)malloc(100 * sizeof(char));
-    for (int i = 0; i < TAMA; i++)
+    for (int i = 0; i < cantNombres; i++)
     {
+        fflush(stdin);
         printf("Ingrese su nombre: ");
         gets(buff);
         *(nombres + i) = (char *)malloc((strlen(buff) + 1) * sizeof(char));
         strcpy(*(nombres + i), buff);
     }
     free(buff);
-    for (int i = 0; i < TAMA; i++)
+    for (int i = 0; i < cantNombres; i++)
     {
         printf("Nombre %d: %s\n", i + 1, *(nombres + i));
     }
